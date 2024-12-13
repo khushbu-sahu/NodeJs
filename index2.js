6199f4870aba82d86baf960c89949c93b869e837
khushbu-sahu <khushboosahu15@outlook.com>
2024-12-13 14:21:41 +0530

    express

---
+++ index2.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port =3000
+
+app.get('/' ,(req , res) => {
+    res.send('hello World')
+})
+
+app.get('/twitter' ,(req, res)=>{
+    res.send('Hello Twitter')
+})
+
+app.get('/login' , (req , res)=>{
+    res.send('<h1>Login Page</h1>')
+
+})
+app.listen(port ,() =>{
+    console.log(`Example app listening on port ${port}`)
+})
+
