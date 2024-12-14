92a2cefc4dae81a3f3f36f3eebeb4db8867ce84a
khushbu-sahu <khushboosahu15@outlook.com>
2024-12-14 16:24:22 +0530

    express

---
+++ Fronted/Backned/server.js
@@ -0,0 +1,44 @@
+// const express= require('express');
+// const app= express();
+
+import express from 'express'
+const app =express();
+
+// app.get('/',(req, res)=>{
+//     res.send('Server is ready');
+// });
+
+
+//get a list of 5 jokes
+
+app.get('/api/jokes' ,(req, res)=>{
+    const jokes=[
+        {
+            id:1,
+            title:'A joke',
+            content:'This is a joke'
+        },
+        {
+            id:2,
+            title:'B joke',
+            content:'This is another joke'
+        },
+        {
+            id:3,
+            title:'C joke',
+            content:'This is one more joke'
+        },
+        {
+            id:4,
+            title:'D joke',
+            content:'This is last joke'
+        },
+    ];
+    res.send(jokes);
+})
+
+const port = process.env.PORT || 4000;
+
+app.listen(port ,()=>{
+    console.log(`Server at http://localhost:${port}`)
+});
--- index2.js
+++ index2.js
@@ -1,6 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port =3000
+// const githubData ={
+//     "login" :"Khushboo sahu",
+//     "id":4
+// }
+
 
 app.get('/' ,(req , res) => {
     res.send('hello World')
@@ -14,7 +20,15 @@ app.get('/login' , (req , res)=>{
     res.send('<h1>Login Page</h1>')
 
 })
-app.listen(port ,() =>{
+
+app.get('/github', (req,res)=>{
+    res.json(githubData)
+})
+// app.listen(port ,() =>{
+//     console.log(`Example app listening on port ${port}`)
+// })
+
+
+app.listen(process.env.PORT ,() =>{
     console.log(`Example app listening on port ${port}`)
 })
-
